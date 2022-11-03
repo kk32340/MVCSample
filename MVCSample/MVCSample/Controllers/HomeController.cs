402f932e73d8ffbbb2d0c981c3200eed4db2f0be
Kalimuthu <k.kalimuthu@gmail.com>
2022-11-03 10:04:23 +0530

    test

---
+++ MVCSample/MVCSample/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCSample.Models;
+using System.Diagnostics;
+using MVCSample.Models;
+
+namespace MVCSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult LoadPartialView(string name)
+        {
+
+            var cust = new Customer()
+            {
+                name = name,
+                phone = "phone1",
+                CustAddresses = new List<Address>()
+                {
+                    {
+                        new Address()
+                        {
+                            city="ciry1",
+                            country="county1",
+                            state="state1",
+                            zipcode="zip1"
+
+                        }
+
+                    }
+                }
+            };
+
+            return PartialView("_customer", cust);
+        }
+
+        public IActionResult Index()
+        {
+
+            var customers = new List<Customer>();
+
+            customers.Add(new Customer()
+            {
+                name = "customer1",
+                phone = "Phone1",
+                CustAddresses = new List<Address>()
+                {
+                    {
+                        new Address()
+                        {
+                            city = "ciry1",
+                            country = "county1",
+                            state = "state1",
+                            zipcode = "zip1"
+
+                        }
+
+                    }
+                }               
+
+
+                 });
+
+
+            customers.Add(new Customer()
+            {
+                name = "customer2",
+                phone = "Phone1",
+                CustAddresses = new List<Address>()
+                {
+                    {
+                        new Address()
+                        {
+                            city = "ciry1",
+                            country = "county1",
+                            state = "state1",
+                            zipcode = "zip1"
+
+                        }
+
+                    }
+                }
+
+
+            });
+
+
+            return View(customers);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCSample/MVCSample/Models/Customers.cs
@@ -0,0 +1,40 @@
+﻿namespace MVCSample.Models
+{
+      
+
+    public class Customer
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public List<Address> CustAddresses { get; set; }
+        public List<AKA> CustAKA { get; set; }
+        public List<Notes> CustNotes { get; set; }
+    }
+
+
+    public class Address
+    {
+
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string country { get; set; }
+           
+    }
+
+    public class AKA
+    {
+        public int id { get; set; }
+        public string name { set; get; }
+        public string phone { set; get; }
+
+    }
+
+    public class Notes
+    {
+        public string notes { get; set; }
+        public string comment { set; get; }
+
+    }
+
+}
